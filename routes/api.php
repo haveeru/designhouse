45239b1292ba01ab979c7922a9530f5b74b22fa4
haveeru <suood_mohamed@hotmail.com>
2020-11-17 19:10:40 +0500

    setting up jwt

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::get('/', function () {
-    return response()->json(['message' => 'Hello World'], 200);
-});
+// Route::get('/', function () {
+//     return response()->json(['message' => 'Hello World'], 200);
+// });
